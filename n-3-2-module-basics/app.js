5230f4a3cad4edd69f5b37a927c73f5d2b59f56c
Tanmay Sinha <tanmay3011@gmail.com>
2017-11-09 16:34:59 +0530

    Require os and fs modules

--- n-3-2-module-basics/app.js
+++ n-3-2-module-basics/app.js
@@ -1,8 +1,23 @@
 console.log('Starting app.');
 
+// require file system module
 const fs = require('fs');
+// require operating system module
 const os = require('os');
 
 var user = os.userInfo();
 
-fs.appendFile('greetings.txt', `Hello ${user.username}!`);
+//Old Syntax
+//fs.appendFile('greetings.txt', `Hello ${user.username}!`);
+
+console.log(user);
+
+//New Syntax adds a option of a callback
+fs.appendFile('greetings.txt', `Hello ${user.username}!`, function (err) {
+  if (err) {
+    console.log('Error Callback: Unable to write to file')
+  }
+});
+
+//Alternate way
+fs.appendFileSync('greetings.txt', `Hello ${user.username}!`);
